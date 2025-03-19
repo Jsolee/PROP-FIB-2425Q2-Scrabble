5c5dbbdfcb7d597fa24a2ac47a0dedd5ef90b105
joan.sole.jimenez <joan.sole.jimenez@estudiantat.upc.edu>
2025-03-19 18:07:10 +0100

    refactor Casella and Fitxa classes to encapsulate fields and add new methods

--- src/main/java/Main/Fitxa.java
+++ src/main/java/Main/Fitxa.java
@@ -1,11 +1,53 @@
 package Main;
 
 public class Fitxa {
-    char letra;
-    int valor;
+    private char lletra;
+    private int valor;
+    private boolean esComodin;
 
-    public Fitxa(char letra, int valor) {
-        this.letra = letra;
+    public Fitxa(char lletra, int valor) {
+        this.lletra = lletra;
         this.valor = valor;
+        this.esComodin = (lletra == '*' || lletra == ' ');
+    }
+
+    // Getters
+    public char getLletra() {
+        return lletra;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public boolean esComodin() {
+        return esComodin;
+    }
+
+    // For blank tiles
+    public void assignarLletra(char novaLletra) {
+        if (esComodin) {
+            this.lletra = novaLletra;
+        }
+    }
+
+    // Reset blank tile
+    public void resetComodin() {
+        if (esComodin) {
+            this.lletra = '*';
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fitxa fitxa = (Fitxa) o;
+        return lletra == fitxa.lletra && valor == fitxa.valor;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(lletra) + "(" + valor + ")";
     }
 }
