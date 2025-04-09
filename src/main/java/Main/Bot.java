7086268743d32d20ef637aec6d2ae6511144cbc8
victor.abello <victor.abello@estudiantat.upc.edu>
2025-04-09 18:09:03 +0200

    add Diccionari class with setters, getters and methods for adding and removing words(those lasts not finished yet)

--- src/main/java/Main/Bot.java
+++ src/main/java/Main/Bot.java
@@ -1,10 +1,13 @@
 package Main;
-
+import Main.Taulell
+import Main.Diccionari
 public class Bot extends Usuari {
     //implementar algorisme aqui?
 
     public Bot(String nom) {
         super(nom);
     }
-    
+
+
+
 }
